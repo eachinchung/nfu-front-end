39115b123403e74c10fff528a54590be5d893815
Eachin <EachinChung@gmail.com>
2019-10-25 19:43:55 +0800

    增加了一些注释

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default config => {
   const instans = axios.create({
-    baseURL: "http://127.0.0.1:5000",
+    baseURL: "http://192.168.1.3:5000",
     timeout: 1000
   })
 
