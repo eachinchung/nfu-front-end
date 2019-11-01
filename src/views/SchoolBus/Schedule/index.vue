d88c55ff7d29da1de76950ed04d676195960cec1
Eachin <EachinChung@gmail.com>
2019-11-01 23:58:30 +0800

    增加校车的api

--- src/views/SchoolBus/Schedule/index.vue
+++ src/views/SchoolBus/Schedule/index.vue
@@ -6,15 +6,28 @@
 
 <script>
     import {beforeRouteCheck} from "@/network/refresh_token";
+    import {schedule} from "@/network/school_bus";
 
     function init(vm) {
         if (vm.$store.state.bus_date == null || vm.$store.state.route_id == null) {
             vm.$router.push("/main/school_bus");
             return null;
         }
+
+        schedule(vm.$store.state.access_token, vm.$store.state.route_id, vm.$store.state.bus_date).then(
+            res=>{
+                console.log(res);
+            }
+        )
+
     }
 
     export default {
+        data() {
+            return {
+                scheduleList: []
+            }
+        },
         beforeRouteEnter(to, from, next) {
             beforeRouteCheck(next, to, init)
         },
