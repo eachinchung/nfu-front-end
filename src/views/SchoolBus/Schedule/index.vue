21ee842ec9a4dde18ef32edd3182aa6f8294d52c
Eachin <EachinChung@gmail.com>
2019-11-01 21:02:49 +0800

    新增班车表

--- src/views/SchoolBus/Schedule/index.vue
+++ src/views/SchoolBus/Schedule/index.vue
@@ -1,120 +1,13 @@
 <template>
-  <div>
-    <van-nav-bar class="title" title="请选择乘车日期" left-arrow @click-left="onClickLeft" />
-
-    <van-cell-group title="选择购买日期" class="group">
-      <van-cell
-        size="large"
-        v-for="item in dateList"
-        :title="item[0]"
-        :value="item[1]"
-        :key="item[0]"
-        is-link
-      />
-    </van-cell-group>
-
-    <van-cell-group>
-      <van-cell size="large" title="预售购票" is-link @click="showPicker=true" />
-    </van-cell-group>
-
-    <van-popup v-model="showPicker" position="bottom">
-      <van-datetime-picker
-        v-model="currentDate"
-        type="date"
-        :min-date="minDate"
-        :max-date="maxDate"
-        @cancel="showPicker=false"
-        @confirm="showPicker=false"
-      />
-    </van-popup>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import {
-  refresh_token,
-  check_refresh_token,
-  handle_token
-} from "@/network/refresh_token";
-
-const WeekDay = [
-  "星期天",
-  "星期一",
-  "星期二",
-  "星期三",
-  "星期四",
-  "星期五",
-  "星期六"
-];
-
-function init(vm) {
-  let loop;
-
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = today.getMonth();
-  const day = today.getDate();
-
-  if (vm.$store.state.route_id == null) {
-    vm.$router.push("/main/school_bus");
-    return null;
-  } else if (vm.$store.state.route_id < 20) {
-    loop = 6;
-    vm.minDate = vm.currentDate = new Date(year, month, day + 5);
-  } else {
-    loop = 2;
-    vm.minDate = vm.currentDate = new Date(year, month, day + 1);
-  }
-
-  vm.maxDate = new Date(year, month, day + 30);
-
-  for (let i = 0; i < loop; i++) {
-    const theDay = new Date(year, month, day + i);
-    vm.dateList.push([
-      theDay.getFullYear() +
-        "-" +
-        (Array(2).join(0) + (theDay.getMonth() + 1)).slice(-2) +
-        "-" +
-        (Array(2).join(0) + theDay.getDate()).slice(-2),
-      WeekDay[theDay.getDay()]
-    ]);
-  }
-}
-
 export default {
-  data() {
-    return {
-      dateList: [],
-      currentDate: new Date(),
-      showPicker: false,
-      minDate: new Date(),
-      maxDate: new Date()
-    };
-  },
-  beforeRouteEnter(to, from, next) {
-    if (check_refresh_token()) {
-      const token = refresh_token();
-      if (token[0]) {
-        next(vm => {
-          token[1].then(
-            res => {
-              if (handle_token(res)) init(vm);
-              else vm.$notify("不可预知错误");
-            },
-            () => vm.$notify("无法连接到服务器")
-          );
-        });
-      } else next({ path: "/login", query: { next: to.fullPath } });
-    } else next(vm => init(vm));
-  },
-  methods: {
-    onClickLeft() {
-      this.$router.push("/main/school_bus");
-    }
-  }
-};
+
+}
 </script>
 
 <style>
-@import "~@/assets/css/common.css";
+
 </style>
