3c8459afb0d9d768098eef6d4e346ce16e60429e
Eachin <EachinChung@gmail.com>
2019-11-03 19:55:54 +0800

    统一样式

--- src/views/Main/Profile/index.vue
+++ src/views/Main/Profile/index.vue
@@ -22,13 +22,13 @@
         icon="envelop-o"
         title="邮箱"
         :value="email"
-        @click="$toast('该功能正在开发中...');"
+        @click="$toast('该功能正在开发中...')"
         is-link
       />
     </van-cell-group>
 
     <van-row type="flex" justify="center" class="row">
-      <van-button type="default" class="button" @click="$toast('该功能正在开发中...');">修改密码</van-button>
+      <van-button type="default" class="button" @click="$toast('该功能正在开发中...')">修改密码</van-button>
     </van-row>
     <van-row type="flex" justify="center">
       <van-button type="warning" class="button" @click="logout">退出登录</van-button>
