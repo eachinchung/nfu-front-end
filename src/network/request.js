ff7e016a019143d7a7305d21981ac8beb914bb78
Eachin <EachinChung@gmail.com>
2019-11-20 03:25:54 +0800

    添加一些注释

--- src/network/request.js
+++ src/network/request.js
@@ -12,6 +12,7 @@ export default config => {
 
   instants.interceptors.request.use(
     config => {
+      // 为每个请求都添加token
       config.headers.Authorization = `Bearer ${store.state.accessToken}`
       return config
     }
@@ -19,6 +20,8 @@ export default config => {
 
   instants.interceptors.response.use(
     response => {
+
+      // 当服务器返1001，token失效，刷新token
       if (response.data.code === "1001") return noToken({
         method: "get",
         url: "oauth/token/refresh",
@@ -29,19 +32,23 @@ export default config => {
         if (res.data.code === "1000") {
           handleToken(res)
           response.config.headers.Authorization = `Bearer ${res.data.message.accessToken}`
+          // 添加新的token后，重新发送请求
           return axios(response.config)
         } else {
+          // 刷新token失败，没救了，重新登录吧
           localStorage.clear()
           router.push({path: "/login", query: {next: router.history.current.fullPath}})
           return response
         }
       }); else return response
+
     }
   )
 
   return instants(config)
 }
 
+// 此为不带token的接口
 export function noToken(config) {
   const instants = axios.create({
     baseURL: process.env.VUE_APP_POST_URL,
