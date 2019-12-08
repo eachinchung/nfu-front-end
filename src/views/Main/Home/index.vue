9817152bdbb20d2baa1fc347b50f28b5e89f77f3
Eachin <EachinChung@gmail.com>
2019-12-08 23:13:05 +0800

    update

--- src/views/Main/Home/index.vue
+++ src/views/Main/Home/index.vue
@@ -1,6 +1,31 @@
 <template>
   <div>
     <van-nav-bar class="title" title="南苑聚合"/>
+
+    <van-cell-group  class="group">
+      <van-cell
+        size="large"
+        icon="bulb-o"
+        title="电费管理"
+        @click="$toast('该功能正在开发中')"
+        is-link
+      />
+      <van-cell
+        size="large"
+        icon="medel-o"
+        title="学分管理"
+        @click="$toast('该功能正在开发中')"
+        is-link
+      />
+      <van-cell
+        size="large"
+        icon="orders-o"
+        title="成绩查询"
+        @click="$toast('该功能正在开发中')"
+        is-link
+      />
+    </van-cell-group>
+
   </div>
 </template>
 
@@ -15,3 +40,6 @@
   }
 </script>
 
+<style scoped>
+  @import "~@/assets/css/common.css";
+</style>
